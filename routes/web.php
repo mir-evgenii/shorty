1b2cd2fd2560e0667e66029b133eb15aa1b8a0be
Evgenii Mironov <mir-evgenii@mail.ru>
2021-10-28 23:20:28 +0300

    Added routes, and turn off verification CSRF token.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/add', function () {
+    return 'New gen short url';
+});
+
+Route::post('/{short_url}', function () {
+    return 'New custom short url';
+});
+
+Route::get('/{short_url}', function () {
+    return 'Reconnect long url';
+});
+
+Route::get('/{short_url}/s', function () {
+    return 'Reconnect long url with button';
+});
